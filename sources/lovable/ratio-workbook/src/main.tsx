65533d831818750f9a4a7d52c95bf426535a0de7
Yaniv Raz <yanivmiz77@gmail.com>
2026-07-19 02:04:43 +0300

    style(יחס): חיבור שכבת הנגישות והזום

--- sources/lovable/ratio-workbook/src/main.tsx
+++ sources/lovable/ratio-workbook/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./ratio-v2.css";
+import "./workbench.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
