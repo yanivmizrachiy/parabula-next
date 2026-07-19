27b29fcca899bb84ca4fd2f31c97916cae9b1a89
Yaniv Raz <yanivmiz77@gmail.com>
2026-07-19 17:20:27 +0300

    build(מערכת צירים): include verified page 36 answers

--- projects/coordinate-segments-areas-workbook/src/all-answers.mjs
+++ projects/coordinate-segments-areas-workbook/src/all-answers.mjs
@@ -3,6 +3,7 @@
 
 import { answers as coreAnswers, glossary } from './answers.mjs';
 import { unit03Answers } from './answers-unit-03.mjs';
+import { page36Answers } from './answers-page36.mjs';
 
-export const answers = [...coreAnswers, ...unit03Answers];
+export const answers = [...coreAnswers, ...unit03Answers, ...page36Answers];
 export { glossary };
