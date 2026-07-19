5f3a26229053ed5f188395d001731c9025ba6907
Yaniv Raz <yanivmiz77@gmail.com>
2026-07-19 17:03:24 +0300

    content(מערכת צירים): include pages 26-35 in workbook sequence

--- projects/coordinate-segments-areas-workbook/src/pages/index.mjs
+++ projects/coordinate-segments-areas-workbook/src/pages/index.mjs
@@ -1,5 +1,6 @@
 // רצף העמודים של החוברת. סדר היחידות קובע את מספור העמודים.
 import unit01 from './unit-01.mjs';
 import unit02 from './unit-02.mjs';
+import unit03 from './unit-03.mjs';
 
-export default [...unit01, ...unit02];
+export default [...unit01, ...unit02, ...unit03];
