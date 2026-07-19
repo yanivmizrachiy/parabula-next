b2018aa4d78a65b21832a3f960d8b51aff40794f
Yaniv Raz <yanivmiz77@gmail.com>
2026-07-19 17:15:30 +0300

    content(מערכת צירים): apply measured enrichment to pages 26-35

--- projects/coordinate-segments-areas-workbook/src/pages/index.mjs
+++ projects/coordinate-segments-areas-workbook/src/pages/index.mjs
@@ -2,5 +2,11 @@
 import unit01 from './unit-01.mjs';
 import unit02 from './unit-02.mjs';
 import unit03 from './unit-03.mjs';
+import unit03Enrichment from './unit-03-enrichment.mjs';
 
-export default [...unit01, ...unit02, ...unit03];
+const enrichedUnit03 = unit03.map(page => ({
+  ...page,
+  blocks: [...page.blocks, ...(unit03Enrichment[page.n] ?? [])]
+}));
+
+export default [...unit01, ...unit02, ...enrichedUnit03];
